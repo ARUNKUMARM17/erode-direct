dab5a33a9bd838b3aaa3f03bf72e7377eb51697f
gitprojects <arunkumarm.22aid@kongu.edu>
2025-04-13 12:07:54 +0530

    made changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static("build"));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "build", "index.html"));
+// 	});
+// }
 // This is your test secret API key.
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 console.log(stripe);
